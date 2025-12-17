9181b943bfa92691d869a5ea754292a1e3b092b2
Manjula-22-mekala <mekalamanjulam@gmail.com>
2025-12-17 18:22:55 +0530

    made request handlers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express=require('express');
 
 const app=express();
 
-app.get('/user',(req,res)=>{
-    res.send({firstname:"Mekala",lastname:"manjula"})
-});
+app.use('/user',
+    [(req,res,next)=>{
+    console.log("Handling the route user");
+    next();
+    //res.send('Response from user route');
+},  (req,res,next)=>{       
+    console.log("Second handler for user route");
+    next();
+   // res.send('Response from second handler of user route');
+}],
+    (req,res,next)=>{
+    console.log("Third handler for user route");
+    next();
+    //res.send('Response from third handler of user route');
+},
+    (req,res)=>{
+    console.log("Fourth handler for user route");
+    res.send('Response from fourth handler of user route');
+}
+);
 
-app.post('/user',(req,res)=>{
-    res.send('Post request called');
-});
 
-app.delete('/user',(req,res)=>{
-    res.send('Delete request called');
-}); 
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
