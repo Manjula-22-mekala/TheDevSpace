8a128a546363f0fe2522f1de302284a46674eb58
Manjula-22-mekala <mekalamanjulam@gmail.com>
2025-12-18 15:17:15 +0530

    data insertion into the database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express=require('express');       
 const connectDB=require('./config/database');
 const User=require('./models/user.js');
+const user = require('./models/user.js');
 const app=express();
 
 app.use(express.json());
@@ -12,6 +13,36 @@ app.post('/signup', async (req,res)=>{
     }
     catch{
         res.status(400).send("Error saving the user:"+err.message);
+    }
+});
+// get user by emailId
+app.get('/user', async (req,res)=>{
+    const userEmail=req.body.emailId;
+    try{
+    
+        const users=await user.find({emailId:userEmail});
+        if (users.length===0){
+            res.status(404).send('User not found'); 
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch{
+        res.status(400).send("Error retrieving the user:"+err.message);
+    }
+})
+
+//get all users from database
+
+app.get('/feed',async(req,res)=>{
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }   
+    catch(err)
+    {
+        res.status(400).send("Error retrieving users:"+err.message);
     }
 });
 
